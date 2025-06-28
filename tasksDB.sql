2c02318b1d9f39332ba48cab6e603428affeb317
nicolas <kochhann.nicolass.soldi@gmail.com>
2025-06-28 16:18:43 -0300

    DB structure

--- tasksDB.sql
+++ tasksDB.sql
@@ -3,11 +3,11 @@ CREATE DATABASE IF NOT EXISTS tasksDB;
 USE tasksDB;
 
 CREATE TABLE tasks (
-    id AUTO_INCREMENT PRIMARY KEY;
-    title VARCHAR(255) NOT NULL;
-    completed BOOLEAN NOT NULL DEFAULT FALSE;
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT FALSE,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 DELIMITER //
 
@@ -20,30 +20,54 @@ BEGIN
     VALUES (taskTitle, taskCompleted);
 END //
 
-DELIMITER ;
 
-
-DELIMITER //
-
-CREATE PROCEDURE selectAllTasks
+CREATE PROCEDURE selectAllTasks()
 BEGIN
     SELECT * FROM tasks;
 END //
 
-DELIMITER ;
 
-
-DELIMITER // 
 
 CREATE PROCEDURE selectTask(
     IN taskId INT
 )
 BEGIN
-        SELECT * FROM tasks WHERE id = taskId;
+        SELECT * FROM tasks 
+        WHERE id = taskId;
 END //
 
-DELIMITER ;
 
-DELIMITER //
 
-CREATE PROCEDURE 
+CREATE PROCEDURE updateTaskTitle(
+    IN taskId  INT,
+    IN taskTitle VARCHAR(255)
+)
+BEGIN
+    UPDATE tasks
+    SET title = taskTitle 
+    WHERE id = taskId;
+END //
+
+
+
+CREATE PROCEDURE updateTaskCompleted(
+    IN taskId INT,
+    IN taskCompleted BOOLEAN
+)
+BEGIN
+    UPDATE tasks
+    SET completed = taskCompleted
+    WHERE id = taskId;
+END //
+
+
+
+CREATE PROCEDURE deleteTask(
+    IN taskId INT
+)
+BEGIN
+    DELETE FROM tasks
+    WHERE id = taskId;
+END //
+
+DELIMITER  ;
